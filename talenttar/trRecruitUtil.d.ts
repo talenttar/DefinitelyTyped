b551e5ea22a2711f6f764d5ab32e83efed2500c8
Jayaprakash Harikrishnan <jayaprakash@talenttar.com>
2016-03-16 19:18:28 +0530

    Libraries declaration updates

--- talenttar/trRecruitUtil.d.ts
+++ talenttar/trRecruitUtil.d.ts
@@ -1,54 +1,44 @@
-/// <reference path="../angular-material/angular-material.d.ts" />
-/// <reference path="../angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="../angularjs/angular-mocks.d.ts" />
-/// <reference path="../angularjs/angular-route.d.ts" />
-/// <reference path="../angularjs/angular-sanitize.d.ts" />
-/// <reference path="../angularjs/angular.d.ts" />
-/// <reference path="../jquery/jquery.d.ts" />
-/// <reference path="../moment/moment-node.d.ts" />
-/// <reference path="../moment/moment.d.ts" />
-/// <reference path="../ng-file-upload/ng-file-upload.d.ts" />
+///<reference path="../angularjs/angular.d.ts"/>
+///<reference path="../angularjs/angular-cookies.d.ts"/>
+///<reference path="../angular-material/angular-material.d.ts"/>
+///<reference path="../angular-ui-router/angular-ui-router.d.ts"/>
+
+declare var API_ENDPOINT: string;
+declare var DEBUG: boolean;
 declare module Util {
     import IServiceProviderClass = angular.IServiceProviderClass;
+    function registerConfigs(configFunction: Function): void;
+    function registerController(name: string, controllerConstructor: Function): void;
     function registerService(name: string, serviceConstructor: Function): void;
+    function registerFactory(name: string, inlineAnnotatedFunction: Function): void;
     function registerProvider(name: string, serviceProviderConstructor: IServiceProviderClass): void;
 }
+declare module Util.Controllers {
+    interface IScope extends ng.IScope {
+        events: AppController;
+    }
+    class AppController {
+        protected scope: IScope;
+        protected window: ng.IWindowService;
+        constructor(scope: IScope, window: ng.IWindowService);
+    }
+}
+declare module Util.Controllers {
+    interface IErrorScope extends IScope {
+        currentYear: string;
+    }
+    class ErrorController extends AppController {
+        protected scope: IErrorScope;
+        protected window: ng.IWindowService;
+        static $inject: string[];
+        constructor(scope: IErrorScope, window: ng.IWindowService);
+        onClickBackButton(): void;
+    }
+}
 declare module Util.Models {
-    interface IStateService {
+    interface IStateService extends ng.ui.IStateService {
         next: ng.ui.IState;
         toParams: ng.ui.IStateParamsService;
-    }
-}
-declare var DEBUG: boolean;
-declare module Util.Providers {
-    class ConfigurationService {
-        private mdThemingProvider;
-        static $inject: any[];
-        constructor();
-        setMdThemingProvider(mdThemingProvider: ng.material.IThemingProvider): void;
-        getMdThemingProvider(): ng.material.IThemingProvider;
-    }
-    class ConfigurationServiceProvider implements ng.IServiceProvider {
-        private locationProvider;
-        private provide;
-        private urlRouterProvider;
-        private mdThemingProvider;
-        static $inject: string[];
-        constructor(locationProvider: ng.ILocationProvider, provide: ng.auto.IProvideService, urlRouterProvider: ng.ui.IUrlRouterProvider, mdThemingProvider: ng.material.IThemingProvider);
-        defaultRoute(path: string): void;
-        configure(): void;
-        $get(): ConfigurationService;
-    }
-}
-declare module Util.Models {
-    interface IRequiredCompatible {
-        browser: Boolean;
-    }
-    interface IOptionalCompatible {
-    }
-    interface ICompatible {
-        required: IRequiredCompatible;
-        optional: IOptionalCompatible;
     }
 }
 declare module Util.Models {
@@ -64,6 +54,64 @@ declare module Util.Models {
         warn: IClientColorDefinition;
         background: IClientColorDefinition;
     }
+    interface IThemingProvider extends ng.material.IThemingProvider {
+        generateThemesOnDemand(onDemand: boolean): any;
+    }
+}
+declare var DEBUG: boolean;
+declare module Util.Providers {
+    import IThemingProvider = Util.Models.IThemingProvider;
+    class ConfigurationService {
+        private mdThemingProvider;
+        static $inject: any[];
+        constructor();
+        setMdThemingProvider(mdThemingProvider: IThemingProvider): void;
+        getMdThemingProvider(): IThemingProvider;
+    }
+    class ConfigurationServiceProvider implements ng.IServiceProvider {
+        private locationProvider;
+        private provide;
+        private urlRouterProvider;
+        private mdThemingProvider;
+        private httpProvider;
+        static $inject: string[];
+        constructor(locationProvider: ng.ILocationProvider, provide: ng.auto.IProvideService, urlRouterProvider: ng.ui.IUrlRouterProvider, mdThemingProvider: IThemingProvider, httpProvider: ng.IHttpProvider);
+        defaultRoute(path: string): void;
+        configure(): void;
+        $get(): ConfigurationService;
+    }
+}
+declare module Util.Models {
+    interface IUtilConstants {
+        ApiEndpoint: string;
+        Debug: boolean;
+        ErrorStates?: any;
+    }
+}
+declare module Util.Models {
+    interface IRequiredCompatible {
+        browser: Boolean;
+    }
+    interface IOptionalCompatible {
+    }
+    interface ICompatible {
+        required: IRequiredCompatible;
+        optional: IOptionalCompatible;
+    }
+}
+declare module Util.Models {
+    interface IClientDetails {
+        id: number;
+        name: string;
+    }
+}
+declare module Util.Configs {
+    import IUtilConstants = Util.Models.IUtilConstants;
+    function StateConfig(stateProvider: ng.ui.IStateProvider, UtilConstants: IUtilConstants): void;
+}
+declare module Util.Interceptors {
+    import IUtilConstants = Util.Models.IUtilConstants;
+    function SecurityInterceptor(injector: ng.auto.IInjectorService, location: ng.ILocationService, UtilConstants: IUtilConstants): ng.IHttpInterceptor;
 }
 declare module Util.Services {
     import ICompatible = Util.Models.ICompatible;
@@ -87,13 +135,47 @@ declare module Util.Services {
     }
 }
 declare module Util.Services {
+    class StateTransitionService {
+        protected rootScope: ng.IRootScopeService;
+        protected state: ng.ui.IStateService;
+        protected timeout: ng.ITimeoutService;
+        protected interval: ng.IIntervalService;
+        protected loading: boolean;
+        protected loadingPercentage: number;
+        private loadingIntervalPromise;
+        static $inject: string[];
+        constructor(rootScope: ng.IRootScopeService, state: ng.ui.IStateService, interval: ng.IIntervalService, timeout: ng.ITimeoutService);
+        isLoading(): boolean;
+        getLoadingPercentage(): number;
+        runTransitionRules(): void;
+    }
+}
+declare module Util.Services.Data {
+    import IClientColorTheme = Util.Models.IClientColorTheme;
+    import IUtilConstants = Util.Models.IUtilConstants;
+    import IClientDetails = Util.Models.IClientDetails;
+    class ClientDataService {
+        protected http: ng.IHttpService;
+        protected UtilConstants: IUtilConstants;
+        static $inject: string[];
+        constructor(http: ng.IHttpService, UtilConstants: IUtilConstants);
+        getClientDetails(): ng.IPromise<IClientDetails>;
+        isCareerPortalEnabled(): ng.IPromise<boolean>;
+        isReferralPortalEnabled(): ng.IPromise<boolean>;
+        getLogoUrl(): string;
+        getFaviconUrl(): string;
+        getColorTheme(): ng.IPromise<IClientColorTheme>;
+    }
+}
+declare module Util.Services {
     import IClientColorTheme = Util.Models.IClientColorTheme;
     import ConfigurationService = Util.Providers.ConfigurationService;
     class ThemeService {
+        protected cookies: ng.cookies.ICookiesService;
         protected mdTheming: any;
         protected ConfigurationService: ConfigurationService;
         static $inject: string[];
-        constructor(mdTheming: any, ConfigurationService: ConfigurationService);
+        constructor(cookies: ng.cookies.ICookiesService, mdTheming: any, ConfigurationService: ConfigurationService);
         applyTheme(clientColorTheme: IClientColorTheme): void;
     }
 }
